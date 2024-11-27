33dd8c3083ec880ebc4e0c670eb12c43c4adc74a
fatihakb01 <fakbulut363@gmail.com>
2024-11-27 21:00:14 +0100

    feat: create seperate class for SNS csv files

--- sns.py
+++ sns.py
@@ -1,27 +1,22 @@
 # Import modules
 import pandas as pd
-from file_manager import FileManager
+from bank_base import BankBase
 
 
-class SNS:
-    def __init__(self, file_manager: FileManager, seperator: str = ';', decimal=',',
-                 encoding='utf-8', engine='openpyxl'):
+class SNS(BankBase):
+    def __init__(self, file_manager, seperator: str = ',', decimal: str = ',', encoding: str = 'utf-8',
+                 engine: str = 'openpyxl'):
         """
-        Initialize the Reader class with a FileManager instance.
+        Initialize the RABO class with specific defaults or custom arguments.
 
         Parameters:
             file_manager (FileManager): Instance of FileManager to manage file paths.
-            seperator (str): Delimiter used in the CSV file. Default is ';'.
+            seperator (str): Delimiter used in the CSV file. Default is ','.
             decimal (str): Decimal separator used in the CSV file. Default is ','.
             encoding (str): Character encoding of the CSV file. Default is 'utf-8'.
             engine (str): Engine used for Excel file writing. Default is 'openpyxl'.
         """
-        self.file_manager = file_manager
-        self.df = None
-        self.seperator = seperator
-        self.decimal = decimal
-        self.encoding = encoding
-        self.engine = engine
+        super().__init__(file_manager, seperator, decimal, encoding, engine)
 
     def load_file(self, file_path: str):
         """
