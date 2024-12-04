061da4cbab3046266648c847fb54bfabd804ac94
fatihakb01 <fakbulut363@gmail.com>
2024-12-04 06:58:12 +0100

    feat: make main.py compatible with the rest of the code

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ def main():
         year = int(input("Enter the year: "))
         month = input("Enter the month: ").strip()
         bank = int(input(f"Choose one of the following banks:"
-                         f"\n{int(1)}. SNS Bank\n{int(2)}. RABO Bank\nI choose option (type in a number): "))
+                         f"\n{int(1)}. SNS Bank\n{int(2)}. RABO Bank\n{int(3)}. Example Bank"
+                         f"I choose option (type in a number): "))
 
         # Initialize FileManager and Reader
         file_manager = None
         reader = None
         if bank == 1:
-            file_manager = FileManager(r"data\sns")
+            file_manager = FileManager("sns", "data", "results")
             reader = SNS(file_manager)
         elif bank == 2:
-            file_manager = FileManager(r"data\rabo")
+            file_manager = FileManager("rabo", "data", "results")
             reader = RABO(file_manager)
         else:
             print("Please provide a valid option (Either option 1 or 2).")
@@ -30,7 +31,7 @@ def main():
 
         # Determine output path and save the Excel file
         output = file_manager.write_new_file(year, f"{month}")
-        reader.csv_to_excel(file_path, output)
+        reader.csv_to_excel(file_path, output, reader.date, reader.name, reader.amount, reader.description, reader.iban)
     except Exception as e:
         print(f"An error occurred: {e}")
 
